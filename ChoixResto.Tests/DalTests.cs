3da70676aebe6a50de88efd6b4362dd5f671aa72
Rabehoavy <rabehoavy@gmail.com>
2019-04-19 11:00:36 +0400

    Create Resto Test

--- ChoixResto.Tests/DalTests.cs
+++ ChoixResto.Tests/DalTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ChoixResto.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ChoixResto.Tests
@@ -7,8 +9,18 @@ namespace ChoixResto.Tests
     public class DalTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreerRestaurant_AvecUnNouveauRestaurant_ObtientTousLesRestaurantsRenvoitBienLeRestaurant()
         {
+            using (IDal dal = new Dal())
+            {
+                dal.CreerRestaurant("La bonne fourchette", "01 02 03 04 05");
+                List<Resto> restos = dal.ObtientTousLesRestaurants();
+
+                Assert.IsNotNull(restos);
+                Assert.AreEqual(1, restos.Count);
+                Assert.AreEqual("La bonne fourchette", restos[0].Nom);
+                Assert.AreEqual("01 02 03 04 05", restos[0].Telephone);
+            }
         }
     }
 }
