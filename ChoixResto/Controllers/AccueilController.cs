615c550742b0a8e9b7a3a322d0488ad2aa8be08a
Rabehoavy <rabehoavy@gmail.com>
2019-04-20 21:13:34 +0400

    View-models

--- ChoixResto/Controllers/AccueilController.cs
+++ ChoixResto/Controllers/AccueilController.cs
@@ -1,8 +1,7 @@
 ﻿using ChoixResto.Models;
+using ChoixResto.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ChoixResto.Controllers
@@ -11,10 +10,28 @@ namespace ChoixResto.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["message"] = "Bonjour depuis le contrôleur";
+            /*ViewData["message"] = "Bonjour depuis le contrôleur";
             ViewData["date"] = DateTime.Now;
             Resto r = new Resto { Nom = "La bonne fourchette", Telephone = "1234" };
-            return View(r);
+            return View(r);*/
+
+            //view-model
+            AccueilViewModel vm = new AccueilViewModel
+            {
+                Message = "Bonjour depuis le <span style=\"color:red\">contrôleur</span>",
+                Date = DateTime.Now,
+                //Un seul Resto
+                Resto = new Resto { Nom = "La bonne fourchette", Telephone = "1234" },
+                //Tous les Restos
+                ListeDesRestos = new List<Resto>
+                {
+                    new Resto { Nom = "Resto pinambour", Telephone = "1234" },
+                    new Resto { Nom = "Resto tologie", Telephone = "1234" },
+                    new Resto { Nom = "Resto ride", Telephone = "5678" },
+                    new Resto { Nom = "Resto toro", Telephone = "555" },
+                }
+            };
+            return View(vm);
         }
     }
 }
---
+++ ChoixResto/ViewModels/AccueilViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.ViewModels
+{
+    public class AccueilViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        //Un seul Resto
+        public Models.Resto Resto { get; set; }
+        //Tous les Restos
+        public List<Models.Resto> ListeDesRestos { get; set; }
+    }
+}
