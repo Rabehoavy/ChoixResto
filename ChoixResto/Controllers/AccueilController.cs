1aa842ecbbf99d207af3c4fc685ae44acef21f96
Rabehoavy <rabehoavy@gmail.com>
2019-04-21 00:10:46 +0400

    Partial view

--- ChoixResto/Controllers/AccueilController.cs
+++ ChoixResto/Controllers/AccueilController.cs
@@ -45,5 +45,18 @@ namespace ChoixResto.Controllers
 
             return View(vm);
         }
+
+        [ChildActionOnly]
+        public ActionResult AfficheListeRestaurant()
+        {
+            List<Models.Resto> listeDesRestos = new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinambour", Telephone = "1234" },
+                new Resto { Id = 2, Nom = "Resto tologie", Telephone = "1234" },
+                new Resto { Id = 5, Nom = "Resto ride", Telephone = "5678" },
+                new Resto { Id = 9, Nom = "Resto toro", Telephone = "555" },
+            };
+            return PartialView(listeDesRestos);
+        }
     }
 }
--- ChoixResto/ViewModels/AccueilViewModel.cs
+++ ChoixResto/ViewModels/AccueilViewModel.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChoixResto.ViewModels
 {
     public class AccueilViewModel
     {
+        [Display(Name = "Le message")]
         public string Message { get; set; }
         public DateTime Date { get; set; }
         //Un seul Resto
         public Models.Resto Resto { get; set; }
         //Tous les Restos
         public List<Models.Resto> ListeDesRestos { get; set; }
+        public string Login { get; set; }
     }
 }
