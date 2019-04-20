411285f7696318f9240cd33ca47c97481574cb46
Rabehoavy <rabehoavy@gmail.com>
2019-04-20 10:04:09 +0400

    ViewData - ViewBag -

---
+++ ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,20 @@
+﻿using ChoixResto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["message"] = "Bonjour depuis le contrôleur";
+            ViewData["date"] = DateTime.Now;
+            Resto r = new Resto { Nom = "La bonne fourchette", Telephone = "1234" };
+            return View(r);
+        }
+    }
+}
