ad3f0300f133f333f9daf5adc54dcb663c1f0a78
Rabehoavy <rabehoavy@gmail.com>
2019-04-20 22:43:28 +0400

    Helpers HTML

--- ChoixResto/Controllers/AccueilController.cs
+++ ChoixResto/Controllers/AccueilController.cs
@@ -31,6 +31,18 @@ namespace ChoixResto.Controllers
                     new Resto { Nom = "Resto toro", Telephone = "555" },
                 }
             };
+
+            //SelectList
+            List<Models.Resto> listeDesRestos = new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinambour", Telephone = "1234" },
+                new Resto { Id = 2, Nom = "Resto tologie", Telephone = "1234" },
+                new Resto { Id = 5, Nom = "Resto ride", Telephone = "5678" },
+                new Resto { Id = 9, Nom = "Resto toro", Telephone = "555" },
+            };
+
+            ViewBag.ListeDesRestos = new SelectList(listeDesRestos, "Id", "Nom", 5);
+
             return View(vm);
         }
     }
